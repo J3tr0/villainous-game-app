bc15f03242d739855c05b8f13a265ece8e19b1da
Fabio Giannace <fabio.giannace@gmail.com>
2025-01-05 12:28:21 +0100

    force update

--- public/sw.js
+++ public/sw.js
@@ -1,38 +1,39 @@
-const CACHE_NAME = 'villainous-v2.2';
+const CACHE_NAME = 'villainous-v2.3';
 
 self.addEventListener('install', (event) => {
-	event.waitUntil(
-		caches.open(CACHE_NAME).then((cache) => {
-			return cache.addAll([
-				'/',
-				'/index.html',
-				'/manifest.json',
-				'/icon-192x192.png',
-				'/icon-512x512.png',
-			]);
-		})
-	);
+	event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', (event) => {
 	event.waitUntil(
-		caches.keys().then((cacheNames) => {
-			return Promise.all(
-				cacheNames.map((cacheName) => {
-					if (cacheName !== CACHE_NAME) {
-						return caches.delete(cacheName);
-					}
-				})
-			);
-		})
+		Promise.all([
+			self.clients.claim(),
+			caches.keys().then((cacheNames) => {
+				return Promise.all(
+					cacheNames.map((cacheName) => {
+						if (cacheName !== CACHE_NAME) {
+							return caches.delete(cacheName);
+						}
+					})
+				);
+			}),
+		])
 	);
 });
 
 self.addEventListener('fetch', (event) => {
 	event.respondWith(
-		caches.match(event.request).then((response) => {
-			return response || fetch(event.request);
-		})
+		fetch(event.request)
+			.then((response) => {
+				const responseClone = response.clone();
+				caches.open(CACHE_NAME).then((cache) => {
+					cache.put(event.request, responseClone);
+				});
+				return response;
+			})
+			.catch(() => {
+				return caches.match(event.request);
+			})
 	);
 });
 
